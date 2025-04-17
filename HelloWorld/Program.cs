4c6ad82d927e3d221247f1f4cdf71fb915111b0e
Majarana <j.marashli@centrum.cz>
2025-04-17 17:36:01 +0200

    Add static method calculate rectangle area

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-
         Car car = new Car();
         car.SetColor("blue");
+
+        int rectangleArea = Rectangle.Calculate(6, 8);
     }
 }
 
@@ -18,5 +19,14 @@ class Car
     public void SetColor(string color){
         this.color = color;
     }
+}
 
+//we want a method that calculates area of rectangle without an instance of class Rectangle
+
+class Rectangle
+{
+    public static int Calculate(int a, int b)
+    {
+      return a*b;
+    }
 }
