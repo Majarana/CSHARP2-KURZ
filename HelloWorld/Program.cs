7a9bad3f0b403d5a41b2aacdb1c1ba82124a452a
Majarana <j.marashli@centrum.cz>
2025-04-17 17:19:27 +0200

    Add class Car and method set his color

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World! :)");
+
+        Car car = new Car();
+        car.SetColor("blue");
     }
+}
+
+//třída Car a metoda, která nastavuje jeho barvu
+class Car
+{
+    private string color;
+
+    public void SetColor(string color){
+        this.color = color;
+    }
+
 }
